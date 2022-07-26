e573c6a770c19c58620d84d4d567a9ef75303889
Dante <zakdante23@gmail.com>
2022-07-26 14:48:46 +0200

    First commit

---
+++ src/Components/SlideBar/SlideBar.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./SlideBar.css";
+import HomeMaxIcon from "@mui/icons-material/HomeMax";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import SettingsIcon from "@mui/icons-material/Settings";
+import QueueIcon from "@mui/icons-material/Queue";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import CloseIcon from "@mui/icons-material/Close";
+import SideItem from "./SideItems/SideItem";
+import { Link } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+
+
+function SlideBar() {
+  const [show, setShow] = React.useState(false);
+
+  return (
+    <>
+      <div className={show ? "sidebar side__show" : "sidebar"}>
+        <Link to="/">
+          {" "}
+          <div className="logo">
+            <img
+              src="https://images.vexels.com/media/users/3/229320/isolated/preview/3dbf158d77c22e31cee5eafbdcf5ce0f-square-gradient-logo.png"
+              alt="logo"
+            />
+            <h3>Zade Cinema</h3>
+          </div>
+        </Link>
+        <div className="side__box">
+          <div className="box__one">
+            <span className="heading">Menu</span>
+            <SideItem Icon={HomeMaxIcon} Name="Acceuil" active link="/" />
+            <SideItem Icon={SearchIcon} Name="Recherche" link="/search" />
+            <SideItem
+              Icon={FavoriteBorderIcon}
+              Name="Mes preferes"
+              link="/favorite"
+            />
+          </div>
+          <div className="box__two">
+            <span className="heading">Parametre</span>
+            <SideItem Icon={SettingsIcon} Name="Configuration" link="/setting" />
+          </div>
+        </div>
+        <div className="menu__icon" onClick={() => setShow(!show)}>
+          <MenuIcon />
+        </div>
+      </div>
+      <div className={show ? 'layer layer__show': 'layer'} onClick={() => setShow(false)}></div>
+    </>
+  );
+}
+
+export default SlideBar;
---
+++ src/Components/Widgets/Widgets.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Widgets.css";
+import SearchIcon from "@mui/icons-material/Search";
+import SearchBar from "./SearchBar/SearchBar";
+import PopularMovies from "./PopularMovies/PopularMovies";
+import { useGetPopularQuery } from "../../features/Api";
+
+function Widgets() {
+  const { data } = useGetPopularQuery();
+  const res = data?.results;
+
+  return (
+    <>
+      <div className="widgets">
+        <SearchBar Icon={SearchIcon} />
+
+        <div className="popular__box">
+          <div className="heading">Recement au Cinémas</div>
+          {res &&
+            res.map((data) => (
+              <PopularMovies
+                Banner={data?.backdrop_path}
+                title={data?.original_title || data?.original_name}
+                id={data.id}
+                description={data?.overview}
+                type="movie"
+              />
+            ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Widgets;
